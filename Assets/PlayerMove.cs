9f5d49935e0b356283db798085a64f7c41455c45
RedWool <leolui0930.sia@gmail.com>
2022-04-16 18:27:37 -0400

    Initial Stuff
    Game Jam

---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        Vector2 move = new Vector2(x, y).normalized;
+
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+        {
+            move *= 0.5f;
+        }
+
+        transform.Translate(move * speed * Time.deltaTime);
+    }
+}
