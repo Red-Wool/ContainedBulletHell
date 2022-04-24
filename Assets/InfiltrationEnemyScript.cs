ccb8f593f6cb8d75c429afac8ecc85254596b50d
Mustan Pathetic <patheticmustan@gmail.com>
2022-04-23 23:49:53 -0400

    why

---
+++ Assets/InfiltrationEnemyScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfiltrationEnemyScript : MonoBehaviour {
+    public float speed = 5;
+    public float bulletSpeed = 8;
+    public float bulletLifetime = 10;
+    public float bulletDelay = 2;
+
+    public GameObject bulletPrefab;
+    private GameObject player;
+    private float bdc;
+
+    void Start() {
+        bdc = bulletDelay;
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update() {
+        bdc -= Time.deltaTime;
+
+        // check if it's time to spawn a new bullet
+        if (bdc <= 0) {
+            bdc = bulletDelay;
+            InfiltrateBulletScript b = Instantiate(bulletPrefab).GetComponent<InfiltrateBulletScript>();
+            b.SetUp(Vector3.MoveTowards(transform.position, player.transform.position).normalized * bulletSpeed, bulletLifetime);
+        }
+    }
+}
